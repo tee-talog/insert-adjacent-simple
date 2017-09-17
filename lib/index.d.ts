70f65910e1fc6260f37b9743a5acc16dc484f86e
talog <tee.talog@gmail.com>
2017-09-17 17:09:37 +0900

    Change compile task

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare function insertHTMLBeforeBegin(element: Element, text: string): void;
+export declare function insertHTMLAfterBegin(element: Element, text: string): void;
+export declare function insertHTMLBeforeEnd(element: Element, text: string): void;
+export declare function insertHTMLAfterEnd(element: Element, text: string): void;
+export declare function insertElementBeforeBegin(targetElement: Element, element: Element): Element | null;
+export declare function insertElementAfterBegin(targetElement: Element, element: Element): Element | null;
+export declare function insertElementBeforeEnd(targetElement: Element, element: Element): Element | null;
+export declare function insertElementAfterEnd(targetElement: Element, element: Element): Element | null;
